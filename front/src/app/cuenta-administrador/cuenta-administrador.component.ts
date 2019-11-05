429ac3a657ba30f48a91493f7ca350363b7a684c
paulis1996 <55591727+paulis1996@users.noreply.github.com>
2019-11-04 22:59:16 -0500

    Actualizacion final cuentaAdministrador.ts

--- front/src/app/cuenta-administrador/cuenta-administrador.component.ts
+++ front/src/app/cuenta-administrador/cuenta-administrador.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from "@angular/core";
+import { Usuario } from '../modelos/usuario.module';
+import { UsuarioService } from '../usuario.service';
+import { LoginService } from '../login.service';
 
 @Component({
   selector: "cuenta-administrador",
@@ -6,7 +9,34 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./cuenta-administrador.component.css"]
 })
 export class CuentaAdministradorComponent implements OnInit {
-  constructor() {}
+  constructor(
+    private _usuarioService: UsuarioService,
+    private _loginService: LoginService
+  ) {}
 
-  ngOnInit() {}
+  listadoUsuarios: Usuario[] = [];
+  listadoOriginal: Usuario[] = [];
+  textoBuscado: string;
+  usuarioActual: Usuario;
+
+  ngOnInit() {
+    this._usuarioService.obtenerUsuarios().subscribe((res: Usuario[]) => {
+      this.listadoUsuarios = res;
+      this.listadoOriginal = res;
+    });
+    this.usuarioActual = this._loginService.obtenerUsuario();
+  }
+
+  eliminarUsuario(id: string) {
+    const usuario = this.listadoUsuarios.find(x => x._id === id);
+    if (confirm(`¿Esta seguro de eliminar el usuario ${usuario.nombre}?`)) {
+      this._usuarioService.eliminarUsuario(id).subscribe(() => {
+        this.listadoUsuarios = this.listadoUsuarios.filter(x => x._id !== id);
+      });
+    }
+  }
+
+  buscar() {
+    this.listadoUsuarios = this.listadoOriginal.filter(x => x.nombre.toLocaleLowerCase().includes(this.textoBuscado.toLocaleLowerCase()));
+  }
 }
