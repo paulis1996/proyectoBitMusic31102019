e3d6ad6e105ddef008a843be64c83f711ccadd5a
paulis1996 <55591727+paulis1996@users.noreply.github.com>
2019-11-04 23:30:36 -0500

    Update app.component.ts

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,14 +1,32 @@
-import { Component, OnInit, OnDestroy} from "@angular/core";
-
+import { Component, OnInit } from "@angular/core";
+import { UsuarioService } from "./usuario.service";
+import { CancionService } from "./cancion.service";
+import { Cancion } from "./modelos/cancion.module";
+import { Usuario } from "./modelos/usuario.module";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = "frontperfil";
-  constructor(){ }
+
+  constructor(
+    private cancionservice: CancionService,
+    private usuarioservice: UsuarioService
+  ) {}
+
+  Canciones: Array<Cancion> = [];
+  Usuarios: Array<Usuario> = [];
+
   ngOnInit() {
-  } 
+    /* this.usuarioservice.getUsuario().subscribe(response => {
+      this.Usuarios = response;
+    }); */
+
+    this.cancionservice.getCanciones().subscribe(response => {
+      this.Canciones = response;
+    });
+  }
 }
